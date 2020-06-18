a11836a109a1fe62f834a60c9b90864ba4914eff
Mike Morel <michael.morel@techelevator.com>
2020-06-18 08:04:28 -0400

    Module 2 capstone starter

---
+++ 09_Capstone/Capstone/Program.cs
@@ -0,0 +1,33 @@
+﻿using CLI;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the connection string from the appsettings.json file
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("Project");
+
+            /********************************************************************
+            // If you do not want to use CLIMenu, you can remove the following
+            *********************************************************************/
+            // Create any DAOs needed here, and then pass them into main menu...
+
+
+            MainMenu mainMenu = new MainMenu();  // You'll probably be adding daos to the constructor
+
+            // Run the menu.
+            mainMenu.Run();
+        }
+    }
+}
---
+++ 09_Capstone/Capstone/Views/MainMenu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CLI
+{
+    /// <summary>
+    /// The top-level menu in our application
+    /// </summary>
+    public class MainMenu : CLIMenu
+    {
+        // You may want to store some private variables here.  YOu may want those passed in 
+        // in the constructor of this menu
+
+        /// <summary>
+        /// Constructor adds items to the top-level menu. You will likely have parameters  passed in
+        /// here...
+        /// </summary>
+        public MainMenu(/* Add any needed parameters here */) : base("Main Menu")
+        {
+            // Set any private variables here.
+        }
+
+        protected override void SetMenuOptions()
+        {
+            // A Sample menu.  Build the dictionary here
+            this.menuOptions.Add("1", "Add 2 integers");
+            this.menuOptions.Add("2", "Ask the user for name");
+            this.menuOptions.Add("3", "Go to a sub-menu");
+            this.menuOptions.Add("Q", "Quit program");
+        }
+
+        /// <summary>
+        /// The override of ExecuteSelection handles whatever selection was made by the user.
+        /// This is where any business logic is executed.
+        /// </summary>
+        /// <param name="choice">"Key" of the user's menu selection</param>
+        /// <returns></returns>
+        protected override bool ExecuteSelection(string choice)
+        {
+            switch (choice)
+            {
+                case "1": // Do whatever option 1 is. You may prompt the user for more information
+                            // (using the Helper methods), and then pass those values into some 
+                            //business object to get something done.
+                    int i1 = GetInteger("Enter the first integer: ");
+                    int i2 = GetInteger("Enter the second integer: ");
+                    Console.WriteLine($"{i1} + {i2} = {i1+i2}");
+                    Pause("Press enter to continue");
+                    return true;    // Keep running the main menu
+                case "2": // Do whatever option 2 is
+                    string name = GetString("What is your name?");
+                    WriteError($"Not yet implemented, {name}.");
+                    Pause("");
+                    return true;    // Keep running the main menu
+                case "3": 
+                    // Create and show the sub-menu
+                    SubMenu1 sm = new SubMenu1();
+                    sm.Run();
+                    return true;    // Keep running the main menu
+            }
+            return true;
+        }
+
+        protected override void BeforeDisplayMenu()
+        {
+            PrintHeader();
+        }
+
+
+        private void PrintHeader()
+        {
+            SetColor(ConsoleColor.Yellow);
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Main Menu"));
+            ResetColor();
+        }
+    }
+}
